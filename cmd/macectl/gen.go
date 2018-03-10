468caa4b310f0011f79e07cf21761802a6f2b991
nico <brun.nicolas21@gmail.com>
2018-03-10 19:44:04 +0100

    rewrite generate

--- cmd/macectl/gen.go
+++ cmd/macectl/gen.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 	"math/big"
 	"net"
+	"os"
 	"path/filepath"
 	"strings"
 	"time"
@@ -18,27 +19,41 @@ import (
 	"github.com/midbel/cli"
 )
 
-type hosts []string
-
-func (h *hosts) String() string {
-	return fmt.Sprint(*h)
+type Time struct {
+	time.Time
 }
 
-func (h *hosts) Set(vs string) error {
+func (t *Time) String() string {
+	return t.Time.String()
+}
+
+func (t *Time) Set(v string) error {
+	i, err := time.Parse(time.RFC3339, v)
+	if err != nil {
+		return err
+	}
+	t.Time = i
+	return nil
+}
+
+type StringArray []string
+
+func (s *StringArray) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *StringArray) Set(vs string) error {
 	for _, v := range strings.Split(vs, ",") {
 		v = strings.TrimSpace(v)
 		if v == "" {
 			continue
 		}
-		*h = append(*h, v)
-	}
-	if len(*h) == 0 {
-		return fmt.Errorf("no hosts provided")
+		*s = append(*s, v)
 	}
 	return nil
 }
 
-type subject struct {
+type Subject struct {
 	Country      string `toml:"country"`
 	State        string `toml:"state"`
 	Locality     string `toml:"locality"`
@@ -48,7 +63,7 @@ type subject struct {
 	Email        string `toml:"email"`
 }
 
-func (s subject) ToName() pkix.Name {
+func (s Subject) ToName() pkix.Name {
 	value := func(s string) []string {
 		if s == "" {
 			return nil
@@ -65,67 +80,123 @@ func (s subject) ToName() pkix.Name {
 	}
 }
 
+type Certificate struct {
+	Name string
+
+	Root   bool
+	CACert string
+	CAKey  string
+
+	Period time.Duration
+	Date   Time
+	Hosts  StringArray
+
+	Curve string
+	Bits  int
+}
+
+func (c Certificate) LoadCA() (*x509.Certificate, *rsa.PrivateKey, error) {
+	var (
+		b   *pem.Block
+		bs  []byte
+		err error
+	)
+	switch bs, err = ioutil.ReadFile(c.CACert); {
+	case err == nil:
+	case os.IsNotExist(err):
+		return nil, nil, nil
+	default:
+		return nil, nil, err
+	}
+	b, _ = pem.Decode(bs)
+	cert, err := x509.ParseCertificate(b.Bytes)
+	if err != nil {
+		return nil, nil, err
+	}
+	if bs, err = ioutil.ReadFile(c.CAKey); err != nil {
+		return nil, nil, err
+	}
+	b, _ = pem.Decode(bs)
+	key, err := x509.ParsePKCS1PrivateKey(b.Bytes)
+	if err != nil {
+		return nil, nil, err
+	}
+	return cert, key, nil
+}
+
+func (c Certificate) Create(s Subject) (*x509.Certificate, *rsa.PrivateKey, error) {
+	key, err := rsa.GenerateKey(rand.Reader, c.Bits)
+	if err != nil {
+		return nil, nil, err
+	}
+	limit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serial, err := rand.Int(rand.Reader, limit)
+	if err != nil {
+		return nil, nil, err
+	}
+	if c.Period == 0 {
+		c.Period = time.Hour * 365 * 24
+	}
+	cert := x509.Certificate{
+		Subject:               s.ToName(),
+		SerialNumber:          serial,
+		NotBefore:             c.Date.Time,
+		NotAfter:              c.Date.Time.Add(c.Period),
+		IsCA:                  c.Root,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+	}
+	if c.Root {
+		cert.KeyUsage |= x509.KeyUsageCertSign
+	}
+	if s.Email != "" {
+		cert.EmailAddresses = []string{s.Email}
+	}
+	for _, h := range c.Hosts {
+		if ip := net.ParseIP(h); ip != nil {
+			cert.IPAddresses = append(cert.IPAddresses, ip)
+		} else {
+			cert.DNSNames = append(cert.DNSNames, h)
+		}
+	}
+	return &cert, key, nil
+}
+
 func runGenerate(cmd *cli.Command, args []string) error {
-	var hs hosts
-	cmd.Flag.Var(&hs, "x", "hosts")
-	stamp := cmd.Flag.String("t", "", "timestamp")
-	days := cmd.Flag.Duration("d", 0, "days")
-	parent := cmd.Flag.String("p", "", "")
-	sign := cmd.Flag.String("k", "", "")
-	bits := cmd.Flag.Int("c", 2048, "")
-	root := cmd.Flag.Bool("r", false, "root ca")
-	name := cmd.Flag.String("n", "mace", "name")
+	var c Certificate
+
+	name, err := os.Hostname()
+	if err == nil {
+		name = "localhost"
+	}
+
+	cmd.Flag.Var(&c.Hosts, "x", "hosts")
+	cmd.Flag.Var(&c.Date, "t", "timestamp")
+	cmd.Flag.DurationVar(&c.Period, "d", 0, "days")
+	cmd.Flag.StringVar(&c.CACert, "p", "", "ca certificate")
+	cmd.Flag.StringVar(&c.CAKey, "k", "", "ca private key")
+	cmd.Flag.IntVar(&c.Bits, "c", 2048, "")
+	cmd.Flag.BoolVar(&c.Root, "r", false, "root ca")
+	cmd.Flag.StringVar(&c.Name, "n", name, "name")
 	if err := cmd.Flag.Parse(args); err != nil {
 		return err
 	}
 	sub := readSubject()
-
-	t, err := createCertificate(*stamp, *days, *root)
+	cert, key, err := c.Create(sub)
 	if err != nil {
 		return err
 	}
-	t.Subject = sub.ToName()
-	if sub.Email != "" {
-		t.EmailAddresses = []string{sub.Email}
-	}
-	for _, h := range hs {
-		if ip := net.ParseIP(h); ip != nil {
-			t.IPAddresses = append(t.IPAddresses, ip)
-		} else {
-			t.DNSNames = append(t.DNSNames, h)
-		}
-	}
-
-	priv, err := rsa.GenerateKey(rand.Reader, *bits)
+	cacert, cakey, err := c.LoadCA()
 	if err != nil {
 		return err
 	}
-
-	other := t
-
-	pkey := priv
-	if bs, err := ioutil.ReadFile(*parent); err == nil {
-		b, _ := pem.Decode(bs)
-		if c, err := x509.ParseCertificate(b.Bytes); err == nil {
-			other = c
-			if other.IsCA {
-				t.Issuer = other.Issuer
-			}
-		} else {
-			return err
-		}
-		if bs, err := ioutil.ReadFile(*sign); err == nil {
-			b, _ := pem.Decode(bs)
-			pkey, err = x509.ParsePKCS1PrivateKey(b.Bytes)
-			if err != nil {
-				return fmt.Errorf("parse signer key: %s", err)
-			}
-		} else {
-			return err
-		}
+	if cacert == nil && cakey == nil {
+		cacert, cakey = cert, key
+	} else {
+		cert.Issuer = cacert.Subject
 	}
 
-	bs, err := x509.CreateCertificate(rand.Reader, t, other, priv.Public(), pkey)
+	bs, err := x509.CreateCertificate(rand.Reader, cert, cacert, key.Public(), cakey)
 	if err != nil {
 		return fmt.Errorf("create cert: %s", err)
 	}
@@ -133,52 +204,20 @@ func runGenerate(cmd *cli.Command, args []string) error {
 	if err := pem.Encode(buf, &pem.Block{Type: "CERTIFICATE", Bytes: bs}); err != nil {
 		return fmt.Errorf("encode cert: %s", err)
 	}
-	if err := ioutil.WriteFile(filepath.Join(cmd.Flag.Arg(0), *name+".pem"), buf.Bytes(), 0600); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(cmd.Flag.Arg(0), c.Name+".pem"), buf.Bytes(), 0600); err != nil {
 		return err
 	}
 	buf.Reset()
 
-	bs = x509.MarshalPKCS1PrivateKey(priv)
+	bs = x509.MarshalPKCS1PrivateKey(key)
 	if err := pem.Encode(buf, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: bs}); err != nil {
 		return fmt.Errorf("encode rsa key: %s", err)
 	}
-	return ioutil.WriteFile(filepath.Join(cmd.Flag.Arg(0), *name+".key"), buf.Bytes(), 0600)
+	return ioutil.WriteFile(filepath.Join(cmd.Flag.Arg(0), c.Name+".key"), buf.Bytes(), 0600)
 }
 
-func createCertificate(s string, d time.Duration, ca bool) (*x509.Certificate, error) {
-	now := time.Now().Truncate(time.Minute * 5)
-	if n, err := time.Parse(time.RFC3339, s); err == nil {
-		now = n
-	} else {
-		return nil, err
-	}
-	if d <= 0 {
-		d = time.Hour * 24 * 365
-	}
-
-	limit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serial, err := rand.Int(rand.Reader, limit)
-	if err != nil {
-		return nil, fmt.Errorf("fail to generate serial number: %s", err)
-	}
-
-	t := x509.Certificate{
-		SerialNumber:          serial,
-		NotBefore:             now,
-		NotAfter:              now.Add(d),
-		IsCA:                  ca,
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		BasicConstraintsValid: true,
-	}
-
-	if ca {
-		t.KeyUsage |= x509.KeyUsageCertSign
-	}
-	return &t, nil
-}
-
-func readSubject() subject {
-	var s subject
+func readSubject() Subject {
+	var s Subject
 
 	fmt.Print("Country Name (2 letter code) []: ")
 	fmt.Scanln(&s.Country)
